50b67d03a8aa887313f79b8ff0f2d331ff818283
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-08 17:17:51 +0900

    done

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,18 @@
+// reactとDOMをインポートする
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// componentを作成する
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const App = function() {
+// アロー関数の場合の例
+    const App = () => {
+    return <div>Hi there!</div>;
+};
+
+// スクリーンに表示する
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')  
+);
+// document.querySelectorでhtmlようその取得
