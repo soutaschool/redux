29ce09e13fa3ea82f887003edbd2835c15551fc2
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-10 16:24:32 +0900

    done

--- components/src/index.js
+++ components/src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 
 // 簡単なコンポーネント
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog text!</div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 
