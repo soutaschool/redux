2541a4c2277c085e8392f6b2a029e926b1a50680
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-12 16:56:12 +0900

    done

--- components/src/index.js
+++ components/src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 // ここをインポートしないと使用することができない
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 // コンポーネントの名前を変えていく
+// <></>都することによって今日つの項目にしたかったという意味になる
 const App = () => {
     return (
         <div className="ui container comments">
+        <ApprovalCard>
         <CommentDetail
             author="sam"
             timeAgo="Today at 4:45PM"
             content="Nice blog post"
             avatar = {faker.image.avatar()}
            />
+        </ApprovalCard>
+
         <CommentDetail 
             author="Alex" 
             timeAgo="Today at 2:00PM" 
