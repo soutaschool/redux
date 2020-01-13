26598899b602ae831643d2d2918486d71b89f90c
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-13 16:28:30 +0900

    done

---
+++ pics/src/components/ SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+// 内部拡張をしていく
+// フォームを作成していく
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
