e6a4dae6cbacc88eb62f273bfe3b15e560789579
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-13 16:37:35 +0900

    done

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import SearchBar from './ SearchBar';
 
+
+// {{}}で行うCSSの指定
 const App = () => {
     return (
-     <div>
+     <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar />
      </div>
     );
