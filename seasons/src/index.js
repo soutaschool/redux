33f3ac29c84f41c3b4b7d07b18a6427e1d16eb58
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-13 14:59:20 +0900

    55

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,8 +5,17 @@ import ReactDOM from 'react-dom';
 
 
 // 新しいクラスを作成（メソッドが一つだけ割り当てられている）
-// 
+// constructorとsuperはセットで覚えるのが吉
+// 新しいインスタンスを作成する
+// latは短縮するのに使用している
+// 状態のオブジェクトを初期化
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
