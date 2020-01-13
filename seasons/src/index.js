1d6e88a2e28df5cf993c06dd47e53760dceca026
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-13 14:36:59 +0900

    done

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// windowの部分はAPIを使用している
-// 現在の位置情報を取得する
-// 見えていなくてもコンソールすることによって表示することが可能
+
 const App = () => {
     window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position),
