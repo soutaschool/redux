aaa589f09c34d99968260f6d3b6c819926b7e960
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-18 17:06:16 +0900

    done

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+
+// ここの部分で初期値の設定
 class SearchBar extends React.Component {
+    state = {term: ''}
+
+    // このようにイベントを利用する
+    onInputChange = () => {
+
+    };
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div className="search-bar ui segment ">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                         type="text"
+                         value={this.state.term}
+                         onChange={this.onInputChange}
+                         />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
