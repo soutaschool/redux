bba618e0dd5609bebfe3fc90b23c5dda29803145
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-18 14:37:40 +0900

    cliant-name-not-find

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,9 +5,18 @@ import SearchBar from './ SearchBar';
 
 
 // Appを継承に変更
+// ここの中にAPIを記入していく
+// asynchじゃ非同期処理を表すもの
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+            Authorization: 'sasaki 32a4479d04cf66b7ab32527b9c932ba9459360b4fa82eb294fa2e8409d0be137'
+        }
+        });
+
+        console.log(response.data.results);
     }
 
 
